f95e9cd0f692c962137ea553ed4be8eafe702ef8
mukuld86 <mukuldeshwal86@gmail.com>
2026-08-17 15:30:27 +0530

    Borrowing and return done. Also adjusted some styles

---
+++ Web/Controllers/BorrowingController.cs
@@ -0,0 +1,72 @@
+﻿using LibraryManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LibraryManagementSystem.Web.Controllers
+{
+    public class BorrowingController : Controller
+    {
+        private readonly IMemberService _memberService;
+        private readonly IBookService _bookService;
+        private readonly IBorrowingService _borrowingService;
+        public BorrowingController(IMemberService memberService, IBookService bookService, IBorrowingService borrowingService)
+        {
+            _memberService = memberService;
+            _bookService = bookService;
+            _borrowingService = borrowingService;
+        }
+
+        public IActionResult Index()
+        {
+            var borrowings = _borrowingService.GetAllBorrowings();
+            return View(borrowings);
+        }
+        [HttpGet]
+        public IActionResult Borrow()
+        {
+            LoadBooksAndMembers();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Borrow(int bookId, int memberId)
+        {
+            bool success = _borrowingService.BorrowBook(bookId, memberId);
+            if (!success)
+            {
+                ModelState.AddModelError("",
+                    "The book cannot be borrowed. Please check if the book is available and the member exists!");
+                LoadBooksAndMembers();
+                return View();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public IActionResult Return(int borrowingId)
+        {
+            bool success = _borrowingService.ReturnBook(borrowingId);
+            if (!success)
+            {
+                return NotFound("Borrowing record not found or the book has already been returned!");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Active()
+        {
+            var borrowings = _borrowingService.GetActiveBorrowings();
+            return View("Index", borrowings);
+        }
+        private void LoadBooksAndMembers()
+        {
+            var books = _bookService.GetAllBooks();
+            var members = _memberService.GetAllMembers();
+            ViewBag.Books = new SelectList(
+                books,
+                "BookId",
+                "Title");
+            ViewBag.Members = new SelectList(
+                members,
+                "MemberId",
+                "MemberName");
+        }
+    }
+}
---
+++ Web/Controllers/MemberController.cs
@@ -0,0 +1,72 @@
+﻿using LibraryManagementSystem.DataAccess.Models;
+using LibraryManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly IMemberService _memberService;
+        public MemberController(IMemberService memberService)
+        {
+            _memberService = memberService;
+        }
+
+        public IActionResult Index()
+        {
+            var members = _memberService.GetAllMembers();
+            return View(members);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(member);
+            }
+            _memberService.AddMember(member);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var member = _memberService.GetMember(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+        [HttpPost]
+        public IActionResult Edit(Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(member);
+            }
+            _memberService.UpdateMember(member);
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var member = _memberService.GetMember(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _memberService.DeleteMember(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
